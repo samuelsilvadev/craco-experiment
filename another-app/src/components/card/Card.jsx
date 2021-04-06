a8a06c1b378186a7f9342b327309db089733e944
Samuel Silva <samuelsilvawb@gmail.com>
2021-04-06 17:15:57 +0100

    feat: Create component library with CRA but build with babel
    - Created another-app
    - Added build step with babel as suggested on https://storybook.js.org/tutorials/design-systems-for-developers/react/en/distribute/
    - Imported library on main-app

---
+++ another-app/src/components/card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Card() {
+    return (
+        <article>
+            <h1>Card</h1>
+        </article>
+    )
+}
--- main-app/src/App.js
+++ main-app/src/App.js
@@ -1,10 +1,12 @@
 import { Button } from 'components-library'
+import { Card } from 'another-app'
 
 function App() {
   return (
    <main>
      <h1>Main App</h1>
      <Button />
+     <Card />
    </main>
   );
 }
